4694249c66db3d249cc1746c5f2c48573784547b
Ahae KIM <kimaae@gmail.com>
2023-06-06 11:03:57 +0900

    color theme sns depth 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,19 @@ module.exports = {
   content: ["./pages/**/*.{js,jsx}", "./components/**/*.{js,jsx}"],
   theme: {
     colors: {
-      facebook: "hsl(208, 92%, 53%)",
-      twitter: "hsl(203, 89%, 53%)",
-      instagram: "linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)",
-      youtube: "hsl(348, 97%, 39%)",
       primary: {
         white: "hsl(0, 0%, 100%)",
         limeGreen: "hsl(163, 72%, 41%)",
         brightRed: "hsl(356, 69%, 56%)",
+      },
+      sns: {
+        facebook: "hsl(208, 92%, 53%)",
+        twitter: "hsl(203, 89%, 53%)",
+        instagram: {
+          left: "hsl(37, 97%, 70%)",
+          right: "hsl(329, 70%, 58%)",
+        },
+        youtube: "hsl(348, 97%, 39%)",
       },
       toggle: {
         dark: "linear gradient hsl(210, 78%, 56%) to hsl(146, 68%, 55%)",
