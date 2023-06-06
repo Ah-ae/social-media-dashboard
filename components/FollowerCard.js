8cdf17b7b75476b1ac7353a48f7ad38bda472c45
Ahae KIM <kimaae@gmail.com>
2023-06-06 11:05:03 +0900

    border gradient 적용할 수 있게 마크업 및 스타일 변경

--- components/FollowerCard.js
+++ components/FollowerCard.js
@@ -1,32 +1,36 @@
 import Image from "next/image";
-import UpOrDown from "./UpOrDown";
+import TrendIndicator from "./TrendIndicator";
 
 function FollowerCard({
   logoPath,
   type,
   account,
   follower,
-  upOrDown,
+  changeType,
   count,
   borderTopColor,
 }) {
   return (
-    <section
-      className={`h-56 p-6 flex flex-col justify-between items-center bg-theme-dark-darkDesaturatedBlue border-t-4 border-solid ${borderTopColor} rounded-md text-theme-dark-desaturatedBlue`}
-    >
-      <div className="flex">
-        <Image src={logoPath} alt={`${type} icon`} width={20} height={20} />
-        <span className="ml-3 text-sm font-bold">{account}</span>
+    <section class="w-4/5 mx-auto">
+      <div class="w-full pt-1.5 rounded-md bg-gradient-to-r from-sns-instagram-left to-sns-instagram-right ">
+        <div class="w-full h-56 p-6 flex flex-col justify-between items-center rounded-b-md bg-theme-dark-darkDesaturatedBlue text-theme-dark-desaturatedBlue">
+          <div className="flex items-center">
+            <Image src={logoPath} alt={`${type} icon`} width={20} height={20} />
+            <span className="ml-3 text-sm font-bold">{account}</span>
+          </div>
+          <div className="flex flex-col items-center">
+            <span className="text-primary-white text-6xl font-bold">
+              {follower}
+            </span>
+            <span className="tracking-wide">{`${
+              type === "instagram"
+                ? "S U B S C R I B E R S"
+                : "F O L L O W E R S"
+            }`}</span>
+          </div>
+          <TrendIndicator changeType={changeType} number={count} unit="Today" />
+        </div>
       </div>
-      <div className="flex flex-col items-center">
-        <span className="text-primary-white text-6xl font-bold">
-          {follower}
-        </span>
-        <span className="tracking-wide">{`${
-          type === "instagram" ? "S U B S C R I B E R S" : "F O L L O W E R S"
-        }`}</span>
-      </div>
-      <UpOrDown upOrDown={upOrDown} number={count} unit="Today" />
     </section>
   );
 }
