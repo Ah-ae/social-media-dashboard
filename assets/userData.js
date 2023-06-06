2abc6f5781ca451dd557eb1f98257788c331ec9b
Ahae KIM <kimaae@gmail.com>
2023-06-06 13:52:22 +0900

    로컬 유저 데이터 작성

---
+++ assets/userData.js
@@ -0,0 +1,94 @@
+export const socialMediaStats = [
+  {
+    socialMediaType: "facebook",
+    account: "@nathanf",
+    follower: "1987",
+    changeType: "up",
+    count: "12",
+    borderTopColor: "bg-sns-facebook",
+  },
+  {
+    socialMediaType: "twitter",
+    account: "@nathanf",
+    follower: "1044",
+    changeType: "up",
+    count: "99",
+    borderTopColor: "bg-sns-twitter",
+  },
+  {
+    socialMediaType: "instagram",
+    account: "@realnathanf",
+    follower: "11k",
+    changeType: "up",
+    count: "199",
+    borderTopColor:
+      "bg-gradient-to-r from-sns-instagram-left to-sns-instagram-right",
+  },
+  {
+    socialMediaType: "youtube",
+    account: "Nathan F.",
+    follower: "8239",
+    changeType: "down",
+    count: "144",
+    borderTopColor: "bg-sns-youtube",
+  },
+];
+
+export const socialMediaMetrics = [
+  {
+    label: "Page Views",
+    socialMediaType: "facebook",
+    number: "87",
+    changeType: "up",
+    percentage: "3",
+  },
+  {
+    label: "Likes",
+    socialMediaType: "facebook",
+    number: "52",
+    changeType: "down",
+    percentage: "2",
+  },
+  {
+    label: "Likes",
+    socialMediaType: "instagram",
+    number: "5462",
+    changeType: "up",
+    percentage: "2257",
+  },
+  {
+    label: "Profile Views",
+    socialMediaType: "instagram",
+    number: "52k",
+    changeType: "up",
+    percentage: "1375",
+  },
+  {
+    label: "Retweets",
+    socialMediaType: "twitter",
+    number: "117",
+    changeType: "up",
+    percentage: "303",
+  },
+  {
+    label: "Likes",
+    socialMediaType: "twitter",
+    number: "507",
+    changeType: "up",
+    percentage: "553",
+  },
+  {
+    label: "Likes",
+    socialMediaType: "youtube",
+    number: "107",
+    changeType: "down",
+    percentage: "19",
+  },
+  {
+    label: "Total Views",
+    socialMediaType: "youtube",
+    number: "1407",
+    changeType: "down",
+    percentage: "12",
+  },
+];
